d2ee5041fe03fc4bbb047bc45f6e1e215343e20d
trofimovamw <trofimovamw@gmail.com>
2021-06-24 11:15:11 +0300

    Move file clean up and indexing to separate rule, adjust Snakefile

--- scripts/binning/run_binning_count.py
+++ scripts/binning/run_binning_count.py
@@ -77,36 +77,14 @@ print('-'*80)
 sam.bin_cal_week()
 
 '''
-Create tsv files containing all bin file names per binning folder and erase empty bins
+Create tsv files containing all bin file names per binning folder
 '''
+# List binnings
 bins = os.listdir(str(bins_dir))
-bins_filter = list(filter(lambda x: not x.startswith('.'), bins))
-
-for folder in bins_filter:
-    files = os.listdir("%s/%s" % (bins_dir, folder))
-    headers = list(filter(lambda x: x.startswith('header'), files))
-    files_filter = list(filter(lambda x: x.startswith('bin'), files))
-    ranges_filter = list(filter(lambda x: x.startswith('range'), files))
-    files_filter.sort()
-    headers.sort()
-    ranges_filter.sort()
-    for i,hfile in enumerate(headers):
-        table = pd.read_table("%s/%s/%s" % (bins_dir, folder, hfile),delimiter='\t',header=0) 
-        if table.empty:
-            bam_name = "%s/%s/%s" % (bins_dir, folder, files_filter[i])
-            header_name = "%s/%s/%s" % (bins_dir, folder, headers[i])
-            range_name = "%s/%s/%s" % (bins_dir, folder, ranges_filter[i])
-            subprocess.check_call("rm %s" % bam_name, shell=True)
-            subprocess.check_call("rm %s" % header_name, shell=True)
-            subprocess.check_call("rm %s" % range_name, shell=True)
-        else:
-            bam_name = "%s/%s/%s" % (bins_dir, folder, files_filter[i])
-            subprocess.check_call("samtools index %s" % bam_name, shell=True)
-
 name_file = "list_of_binnings.tsv"
 file_path = "%s/%s" % (bins_dir, name_file)
 with open(file_path, 'w+', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter='\t',
                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        for i in range(len(bins_filter)):
-            writer.writerow([bins_filter[i]])
+        for i in range(len(bins)):
+            writer.writerow([bins[i]])
