177055943d7900c388a87fab3175e253974f8e05
Maureen Smith <maureen.smith@gmx.de>
2021-06-24 14:55:04 +0200

    again adding haplotye and number mutants to the output

--- scripts/metrics/run_fp.py
+++ scripts/metrics/run_fp.py
@@ -166,22 +166,22 @@ for folder in binnings:
 
 
     # Write bin file
-    name_table = "table_%s_%s_phi_estimate_var_from_size.tsv" % (file_suffix, folder)
-    table_path = "%s/%s" % (out_dir, name_table)
+    name_table = "table_"+file_suffix+"_"+folder+"_phi_estimate_var_from_size.tsv"
+    table_path = str(out_dir) + '/' + name_table
     with open(table_path, 'w+', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter='\t',
                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
         writer.writerow(["date","value","variance_size","variance_mle","num_seqs","times",
-                         "num_days_per_bin", "median_date", "min_date", "max_date"])
+                         "num_days_per_bin", "median_date", "min_date", "max_date", "haplotypes", "num_mut"])
         for i in range(len(weeks)):
             writer.writerow([mean_header_bin[i], thetas[i], variance_size[i], variance[i], num_seqs[i], times[i],
-                             num_days_per_bin[i], median_dates[i], min_dates[i], max_dates[i]])
+                             num_days_per_bin[i], median_dates[i], min_dates[i], max_dates[i], origins[i], num_mut[i]])
     print("      Done.\n")
     # Write to merged bins dataset
     for i, date in enumerate(mean_header_bin):
         if not i+1 == len(mean_header_bin):
             bin_merging_data.append((mean_header_bin[i], thetas[i], variance_size[i], variance[i], num_seqs[i],
-                                     times[i], num_days_per_bin[i], median_dates[i], max_dates[i], folder))
+                                     times[i], num_days_per_bin[i], median_dates[i], max_dates[i], folder, origins[i], num_mut[i]))
 
 
 print("\n      Making the final results table...")
@@ -190,19 +190,19 @@ bin_merging_data_ = sorted(bin_merging_data)
 times = np.arange(0, len(bin_merging_data_))
 name_table = "table_merged_phi_estimates_var_from_size.tsv"
 
-table_path = "%s/%s" % (out_dir, name_table)
+table_path = str(out_dir) + '/' + name_table
 
 with open(table_path, 'w+', newline='') as csvfile:
     writer = csv.writer(csvfile, delimiter='\t',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
     writer.writerow(["t","value","variance","meanBinDate","sampleSize",
-                     "daysPerBin", "medianBinDate", "maxBinDate", "binning"])
+                     "daysPerBin", "medianBinDate", "maxBinDate", "binning", "haplotypes", "numMut"])
     for i in range(len(times)):
         # If bin size==1/variance is bigger or equal to min_bin_size
         # if maxsize, minsize satisfied
         if bin_merging_data_[i][2]<=1/int(min_bin_size) and bin_merging_data_[i][6]>=min_days_span and bin_merging_data_[i][6]<=max_days_span:
             writer.writerow([times[i],bin_merging_data_[i][1],bin_merging_data_[i][2], bin_merging_data_[i][0],
                             bin_merging_data_[i][4],bin_merging_data_[i][6],bin_merging_data_[i][7],
-                            bin_merging_data_[i][8],bin_merging_data_[i][9]])
+                            bin_merging_data_[i][8],bin_merging_data_[i][9],bin_merging_data_[i][10], bin_merging_data_[i][11]])
 
 print("Done.\n")
