233a9183bb26b65a949381a3ae1d152b2404dbc3
trofimovamw <trofimovamw@gmail.com>
2021-07-08 11:45:19 +0200

    Add test sequence sets, modify test scripts

--- baseginpipepy/tests/bam_to_fingerprints_test.py
+++ baseginpipepy/tests/bam_to_fingerprints_test.py
@@ -1,15 +1,29 @@
 import unittest
 from ginpipepy.bam_to_fingerprints import SAMtoFP
 
-# [('WS|500||7d63c560-6ece-4afe-bc53-80bd827cddd0|2020-01-16', '45>A-51>A-56>A-85>G-124>A-126>C-194>A')]
+"""
+Created on Tue Jul 9 2021
 
-class TestFP(test_bam,test_reference,test_reference_name):
+@authors: Maria Trofimova
+"""
 
-    def testBinning(self):
-        sam_to_fp = SAMtoFP(path,reference,refname)
+class testFP(test_bam,test_reference,test_reference_name):
+
+    def __init__(self, bam_path, reference)
+        self.bam_path = bam_path
+        self.reference = reference
+
+    def testFingerprint(self):
+        #Reference name
+        with open(str(reference), "r") as file:
+            header = file.readline()
+        refname = header.strip(">")
+        refname = refname.strip("\n")
+
+        sam_to_fp = SAMtoFP(self.path,self.reference,refname)
         seq_lbase, seq_posbase, lref, mutants_pairs_list = sam_to_fp.writeFP()
         fp_result = '45>A-51>A-56>A-85>G-124>A-126>C-194>A'
-        self.assertEqual(seq_lbase[0],value, fp_result, "Wrong string")
-
-if __name__ == '__main__':
-    unittest.main()
+        print("Correct sequence fingerpint:")
+        print(fp_result)
+        print("Sequence fingerprint in test sequence")
+        print(seq_lbase[0].value)
